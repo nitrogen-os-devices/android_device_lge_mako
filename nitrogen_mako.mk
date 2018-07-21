7e9ea5d26cfd6948f2f37d61063f2f9ef9b09e62
nitin1438 <nitin.chobhe@gmail.com>
2018-07-21 09:25:30 +0530

    mako: Changes for Nitrogen OS

--- nitrogen_mako.mk
+++ nitrogen_mako.mk
@@ -1,20 +1,19 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
-TARGET_BOOTANIMATION_HALF_RES := true
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from hardware-specific part of the product configuration
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/lge/mako/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := lineage_mako
+PRODUCT_NAME := nitrogen_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
